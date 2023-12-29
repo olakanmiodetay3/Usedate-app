26909b4ffd6db33755e2de9eabd690eace900000
Favour Ukonu <favouroukonu@gmail.com>
2023-12-29 13:27:51 +0100

    Fixed a bug - added a name attribute to the input element. Returned some variables for a function.

--- src/components/UseDate.vue
+++ src/components/UseDate.vue
@@ -1,69 +1,65 @@
- 
 <template>
+  <div>
+    <p>Date: {{ dates }}</p>
     <div>
-  
-      <p>Date: {{ dates }}</p>
-      <div>
-        Add Date:
-        <input type="text" @change="handleInputChange" />
-        <br />
-        Add Month:
-        <input type="text" @change="handleInputChange" />
-        <br />
-        <button @click="handleClick">Calculate Date</button>
-      </div>
+      Add Date:
+      <input type="text" name="day" @change="handleInputChange" />
+      <br />
+      Add Month:
+      <input type="text" name="month" @change="handleInputChange" />
+      <br />
+      <button @click="handleClick">Calculate Date</button>
     </div>
-  </template>
-  
-  <script setup>
-  import {ref} from 'vue';
-  import useDate from "./composables/useDate";
-  const { date, getDay, getMonth, addDay, addMonth } = useDate();
-  
-  const daysToAdd = ref("");
-  const monthsToAdd = ref("");
-  // const heading = ref("Current Date");
-  
-   const dates = ref(date.toString());
+  </div>
+</template>
 
-  // const dates = ref(JSON.stringify(date));
-  
-  const handleInputChange = (event) => {
-    let { name, value } = event.target
-    if (name === "day") {
-      if (!value) value = "0";
-      daysToAdd.value = parseInt(value);
-    } else if (name === "month") {
-      if (!value) value = "0";
-      monthsToAdd.value = parseInt(value);
-    }
-  };
-  
-  const handleAddDaysAndMonths = () => {
-    const newDate = addMonth(monthsToAdd.value, addDay(daysToAdd.value, date));
-    return newDate;
-  };
-  
-  const handleClick = () => {
+<script setup>
+import { ref } from "vue";
+import useDate from "../composables/useDate";
+const { date, getDay, getMonth, addDay, addMonth } = useDate();
+
+const daysToAdd = ref("");
+const monthsToAdd = ref("");
+// const heading = ref("Current Date");
+
+const dates = ref(date.toString());
+
+// const dates = ref(JSON.stringify(date));
+
+const handleInputChange = (event) => {
+  // You were trying to grab/read "name" from the input element but your input element doesn't have a name attribute, so I added a name attribute that matches the name === "day" and name === "month" in the handleInputChange function in your template.
+
+  let { name, value } = event.target;
+  if (name === "day") {
+    if (!value) value = "0";
+    daysToAdd.value = parseInt(value);
+  } else if (name === "month") {
+    if (!value) value = "0";
+    monthsToAdd.value = parseInt(value);
+  }
+};
+
+const handleAddDaysAndMonths = () => {
+  const newDate = addMonth(monthsToAdd.value, addDay(daysToAdd.value, date));
+  return newDate;
+};
+
+const handleClick = () => {
+  console.log("clicked");
   const result = handleAddDaysAndMonths();
+  dates.value = result.toString();
 
   if (result instanceof Date) {
-    dates.value = result.toLocaleDateString();
+    // This is commented out because after clicking the button, it updates the date to something like 12/13/2023, instead of the full version. You can uncomment to see how it works.
+    // dates.value = result.toLocaleDateString();
   } else {
     console.error("handleAddDaysAndMonths() did not return a valid date");
   }
 };
 
-  
-  // const handleClick = () => {
-  //   dates.value = handleAddDaysAndMonths();
-  // };
-  </script>
-  
-  <style scoped></style>
-   
+// const handleClick = () => {
+//   dates.value = handleAddDaysAndMonths();
+// };
+</script>
 
-
-    
-
-    
+<style scoped></style>
